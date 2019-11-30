febfd53d95cb8c593595b4dc3b07cb0a13f56b52
Amershabou <amershabou@gmail.com>
2019-11-30 13:13:44 -0500

    Remove unused ReactDOM imports

--- client/components/ImageInSlide.jsx
+++ client/components/ImageInSlide.jsx
@@ -1,6 +1,5 @@
 
 import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
 
 const ImageInSlide = (props) => (
   <div className="carousel-item">
--- client/components/ImagesSlide.jsx
+++ client/components/ImagesSlide.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
 import ImageInSlide from './ImageInSlide.jsx'
 
 const ImagesSlide = (props) => (
