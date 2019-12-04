af23a57703d3be83911ac65fa031b80352c9c622
Amershabou <amershabou@gmail.com>
2019-12-03 20:11:04 -0500

    Added api endpoint for Client and Server

--- server/index.js
+++ server/index.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const path = require('path');
-const parser = require('body-parser')
+const parser = require('body-parser');
+const Image = require('./database/db');
+const retrieve = require('./database/db');
+const morgan = require('morgan');
+
+
+
 let app = express();
 const PORT = process.env.PORT || 3002;
 
@@ -8,8 +14,13 @@ const PORT = process.env.PORT || 3002;
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '/../public')));
 app.use(parser.urlencoded({ extended: true }));
+app.use(morgan('combined'));
 
+app.get('/images/:productId', (req, res) => {
+  let productId = req.params.productId;
+  Image.retrieve(productId).then(data => res.send(data[0].images));
+});
 
 app.listen(PORT, () => {
-  console.log(`listening on port ${PORT}`)
+  console.log(`listening on port ${PORT}`);
 });
